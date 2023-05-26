72b1ef399cd74e28fd2de0faf86da94e6c623a81
Vakoum <filipkuzmicz@yahoo.com>
2023-05-26 10:21:04 +0200

    Employee vars

--- ProjektAnwesendheitssystem/src/PreOrganized/Employee.java
+++ ProjektAnwesendheitssystem/src/PreOrganized/Employee.java
@@ -1,5 +1,11 @@
 package PreOrganized;
 
 public class Employee {
-
+	private int employeeID;
+	private String firstName;
+	private String lastName;
+	private String password;
+	private Double totalWorkTime;
+	private boolean is_admin;
+	
 }
