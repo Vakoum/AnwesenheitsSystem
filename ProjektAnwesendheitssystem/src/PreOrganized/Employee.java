955f420eb644361cdcd06056dd4cb076753a4b91
Vakoum <filipkuzmicz@yahoo.com>
2023-05-28 13:11:49 +0200

    getterSetterEmployee

--- ProjektAnwesendheitssystem/src/PreOrganized/Employee.java
+++ ProjektAnwesendheitssystem/src/PreOrganized/Employee.java
@@ -19,5 +19,23 @@ public class Employee {
 	public int getEmployeeID() {
 		return employeeID;
 	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public boolean getIs_admin() {
+		return is_admin;
+	}
 	
 }
